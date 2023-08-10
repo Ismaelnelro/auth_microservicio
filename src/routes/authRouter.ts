79196644be9d34a6f036143c34e44b7a6da192cc
Ismael <76495548+Ismaelnelro@users.noreply.github.com>
2023-08-10 17:35:41 -0300

    Update authRouter.ts

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -3,7 +3,7 @@ import { loginUser, registerUser } from "../controllers/auth.controller";
 import path from "path";
 
 const authRouter = Router()
-const basePath = '/api/v1/auth/'
+const basePath = '/api/v1/auth'
 
 //INFORMATIVE ROUTE
 authRouter.get('/', (req: Request, res: Response) => {
